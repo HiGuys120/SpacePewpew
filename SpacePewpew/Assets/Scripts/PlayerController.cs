a3c8f64d05bda311e092f2caa65a9d802bd7f77d
00Mugen00 <genericblue36@gmail.com>
2019-02-13 20:42:38 +0100

    Player Movement added

---
+++ SpacePewpew/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 10f;
+    [SerializeField]
+    float padding = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera camera = Camera.main;
+        // ViewPortToWorldPoint() converts the position of something as it relates to the camera view, into a world space value
+        xMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = camera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = camera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = camera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    private void Move()
+    {
+        // Input.GetAxis("Horizontal") can refer to the buttons (<-,->), (a,d) and (left joystick,right joystick) buttons
+        // Time.deltaTime let's slow and fast computers work on same speed (the moveSpeed)
+        float deltaX = Input.GetAxis("Horizontal")*Time.deltaTime*moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
