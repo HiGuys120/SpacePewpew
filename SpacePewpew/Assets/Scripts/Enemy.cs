4dbc2ce788e9a3228616916967c2c325fb516852
HiGuys120 <johncarlo.purihin@gmail.com>
2019-06-27 23:16:42 +0200

    Destroy enemy

---
+++ SpacePewpew/Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float health = 100;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
